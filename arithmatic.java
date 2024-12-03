f1e3495c171a1bf97c9566948456faf5eb146f53
sad54545675 <tayyibafathima666@gmail.com>
2024-12-03 10:31:41 +0530

    Add files via upload

---
+++ arithmatic.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class arithmatic{
+    public static void main(String[] args){
+        int a=22;
+        int b=6;
+        Scanner sc = new Scanner(System.in);
+        a=sc.nextInt();
+        b=sc.nextInt();
+        System.out.println(a+b); 
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+    }
+}
---
+++ relational.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class relational{
+    public static void main(String[] args){
+        int a=12;
+        int b=5;
+        Scanner sc = new Scanner(System.in);
+        a=sc.nextInt();
+        b=sc.nextInt();
+        System.out.println(a==b);
+        System.out.println(a!=b);
+        System.out.println(a>=b);
+        System.out.println(a<+b);
+        System.out.println(a<b);
+        System.out.println(a>b);
+    }
+}
