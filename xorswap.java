9904cda8bd86d4c3fc32a2bfaff3ab13dc3a24d3
HKGN6669 <tayyibafathima666@gmail.com>
2024-12-03 14:20:23 +0530

    Add files via upload

---
+++ xorswap.java
@@ -0,0 +1,13 @@
+import java.util.*;
+  public class xorswap{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        a=a^b;
+        b=a^b;
+        a=a^b;
+        String res=String.format("After swaping\na=%d\nb=%d",a,b);
+        System.out.println(res);
+    }
+  }
