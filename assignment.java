f1dab073926e1e15524d66fb62039d4da7d56887
sad54545675 <tayyibafathima666@gmail.com>
2024-12-03 11:03:01 +0530

    Add files via upload

---
+++ assignment.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class assignment{
+    public static void main(String[] args){
+        int a=12;
+        Scanner sc = new Scanner(System.in);
+        a=sc.nextInt();
+        System.out.println(a+=6);
+        System.out.println(a-=30);
+        System.out.println(a*=4);
+        System.out.println(a/=10);
+        System.out.println(a%=11);
+    }
+}
